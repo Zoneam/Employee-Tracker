5478c1ebcf44de86b36c9b8f5ea5da4fb2c63ab8
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-06-15 14:45:13 -0700

    adding database

---
+++ db/employee_db.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT,
+  name varchar(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title varchar(30) NOT NULL,
+  salary decimal(10,4) NOT NULL,
+  department_id INT NOT NULL,
+  CONSTRAINT `department_id` FOREIGN KEY (department_id) REFERENCES departments (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id int DEFAULT NULL,
+  manager_id int DEFAULT NULL,
+  CONSTRAINT `manager_id` FOREIGN KEY (manager_id) REFERENCES employees (id) ON UPDATE CASCADE,
+  CONSTRAINT `role_id` FOREIGN KEY (role_id) REFERENCES roles (id) ON UPDATE CASCADE
+); 
